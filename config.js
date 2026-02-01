4a192f3c27106a90a226a547b4433ca20acc82f0
Grin60 <95948928+Grin60@users.noreply.github.com>
2026-01-31 22:22:14 -0300

    config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://www.youtube.com/watch?v=Ar48yzjn1PE&list=RDAr48yzjn1PE&start_radio=1", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dmze12s1b/video/upload/v1769752783/nti3iainplzadfge5s8i.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
